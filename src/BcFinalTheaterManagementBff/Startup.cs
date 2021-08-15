b362f144585e5cd8b3fd3ef5960ec246d7fd9045
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-08-15 10:20:21 -0500

    For local development, CORS is wide open.

--- src/BcFinalTheaterManagementBff/Startup.cs
+++ src/BcFinalTheaterManagementBff/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BcFinalTheaterManagementBff
 {
@@ -31,6 +25,7 @@ namespace BcFinalTheaterManagementBff
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BcFinalTheaterManagementBff", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +36,12 @@ namespace BcFinalTheaterManagementBff
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BcFinalTheaterManagementBff v1"));
+
+                app.UseCors(builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    );
             }
 
             app.UseRouting();
